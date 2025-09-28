1de310e3c7c5362ffae3ec83b2f1b480e3ca0ffe
doodlo <leejiahao199506@gmail.com>
2025-09-28 16:06:12 +0800

    fix: 测试本地git 提交信息生效

--- apps/pc/src/App.tsx
+++ apps/pc/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-         666666666666666 <code>src/App.tsx</code> and save to test HMR
+          <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
